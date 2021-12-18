827c4d5895131b047d619154b1b6531192910a17
Rogue <mohithgr@gmail.com>
2021-12-18 23:00:14 +0530

    Todo_app

---
+++ tempData.js
@@ -0,0 +1,71 @@
+export default tempData = [
+    {
+        id:1,
+        name:"Plan a Trip",
+        color:"#24A6D9",
+        todos:[
+            {
+                title:"Book a Flight",
+                completed:false
+            },
+            {
+                title:"Passport Check",
+                completed:true
+            },
+            {
+                title:"Reserve a Hotel Room",
+                completed:true
+            },
+            {
+                title:"Pack Luggage",
+                completed:false
+            }
+        ]
+    },
+    {
+        id:2,
+        name:"Errands",
+        color:"#8022D9",
+        todos:[
+            {
+                title:"Buy Milk",
+                completed:false
+            },
+            {
+                title:"Plan Weekend Trip",
+                completed:true
+            },
+            {
+                title:"Run 2 miles",
+                completed:true
+            },
+            {
+                title:"Make a Video",
+                completed:true
+            },
+            {
+                title:"Take Dog for a walk",
+                completed:true
+            }
+        ]
+    },
+    {
+        id:3,
+        name:"Birthday Party",
+        color:"#595BD9",
+        todos: [
+            {
+                title:"Get Balloons",
+                completed:false
+            },
+            {
+                title:"Send Invitations",
+                completed:true
+            },
+            {
+                title:"Make Dinner Reservations",
+                completed:true
+            }
+        ]
+    }
+];
